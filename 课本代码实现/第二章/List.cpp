acd32b9d90e5769277ca4e6ba55add6cad4beadc
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-03 16:35:30 +0800

    添加大量注释

--- 课本代码实现/第二章/List.cpp
+++ 课本代码实现/第二章/List.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 
-//函数所返回的错误代码
+// 函数所返回的错误代码
 #define TRUE        1
 #define FALSE       0
 #define OK          1
@@ -90,7 +90,7 @@ Status SetValue (List &L, int i, int value)
     }
 }// PutValue
 
-//返回表中第一个与 a 满足关系 compare 的数据元素的位序（从 1 开始）。若这样的数据元素不存在，则返回值为 0。
+// 返回表中第一个与 a 满足关系 compare 的数据元素的位序（从 1 开始）。若这样的数据元素不存在，则返回值为 0。
 int LocateElem (const List &L, int a, Status (*compare)(int, int))
 {
     int i = 1, len = ListLength(L);
@@ -103,19 +103,19 @@ int LocateElem (const List &L, int a, Status (*compare)(int, int))
     else            return 0;
 }// LocateElem
 
-//判断前一个数是否比后一个数大
+// 判断前一个数是否比后一个数大
 Status Bigger (int x, int y)
 {
     return (x > y)?TRUE:FALSE;
 }// Bigger
 
-//判断前一个数是否比后一个数小
+// 判断前一个数是否比后一个数小
 Status Smaller (int x, int y)
 {
     return (x < y)?TRUE:FALSE;
 }// Bigger
 
-//判断前一个数是是否等于后一个数
+// 判断前一个数是是否等于后一个数
 Status Equal (int x, int y)
 {
     return (x == y)?TRUE:FALSE;
@@ -125,20 +125,23 @@ int main()
 {
     List lp;
     int  ans = 0;
-    Status (*equal)(int, int);
-    equal = Equal;
-    InitList(lp, 10, 2);
-    if(IsEmpty(lp))
+    Status (*equal)(int, int) = Equal;              // 定义一个函数指针。该指针使用函数 Equal 来赋值
+    Status (*smaller)(int, int) = Smaller;          // 定义一个函数指针。该指针使用函数 Smaller 来赋值    
+    Status (*bigger)(int, int) = Bigger;            // 定义一个函数指针。该指针使用函数 Bigger 来赋值    
+
+    InitList(lp, 10, 2);                            // 初始化一个线性表
+    if(IsEmpty(lp))                                 // 判断一下这个表是不是空的 
         printf("This list is empty.\n");
     printf("The length of this list is : %d.\n", ListLength(lp));
-    if(GetElem(lp, 3, ans))
+    if(GetElem(lp, 3, ans))                         // 获取第三个元素的值
         printf("The third element's value of this list is : %d.\n", ans);
-    SetValue(lp, 2, 11);
+    SetValue(lp, 2, 11);                            // 将第二个元素的值设置为 11
     printf("The 2nd element's value of this list is : %d.\n", lp[1]);
 
+    //使用定位函数，结合函数指针来获得符合条件的元素的位序
     printf("The location of the first \"11\" is : %d.\n", LocateElem(lp, 11, equal));
 
-    DestroyList(lp);
+    DestroyList(lp);                                // 销毁该线性表
 
     return 0;
 }
