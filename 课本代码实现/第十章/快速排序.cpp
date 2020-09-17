3d06ede1761dfe5fd5cba6efb6d263161ecac12d
wuguangkai211 <wuguangkai2018@outlook.com>
2020-09-17 10:28:27 +0800

    dent

--- 课本代码实现/第十章/快速排序.cpp
+++ 课本代码实现/第十章/快速排序.cpp
@@ -5,7 +5,7 @@
 // 分治法 + 挖坑填坑
 int Partition(int *raw, int low, int high)
 {
-    int pivot = raw[low];       // 设置基点值
+    int pivot = raw[low];                       // 设置基点值
     while(low < high)
     {
         while(low < high && raw[high] >= pivot) // 元素不需要改变位置
@@ -21,8 +21,8 @@ int Partition(int *raw, int low, int high)
 
 void QSort(int *raw, int low, int high)
 {
-    if(low >= high) return; // 指定长度非法
-    int pivot_location = Partition(raw, low, high); // 将数组一分为二
+    if(low >= high) return;                            // 指定长度非法
+    int pivot_location = Partition(raw, low, high);    // 将数组一分为二
     QSort(raw, low, pivot_location - 1);               // 低子表递归
     QSort(raw, pivot_location + 1, high);              // 高子表递归
 }
@@ -37,12 +37,13 @@ int main()
     raw[4] = 1000;
 
     QSort(raw, 0, 4);
+
     for(int i = 0; i< 5; i++)
     {
         printf("%d\n", raw[i]);
     }
 
-
     return 0;
 }
 
+// 推荐博文：https://blog.csdn.net/morewindows/article/details/6684558
