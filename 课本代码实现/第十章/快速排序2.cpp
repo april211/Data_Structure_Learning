c763c3c55fc62021c427055da2f4d0879095cf72
wuguangkai211 <wuguangkai2018@outlook.com>
2020-11-05 17:13:12 +0800

    添加注解

--- 课本代码实现/第十章/快速排序2.cpp
+++ 课本代码实现/第十章/快速排序2.cpp
@@ -14,29 +14,29 @@ void Swap(Type &a, Type &b)
 }
 
 template<class Type>
-int Partition(Type a[], int p, int r)
+int Partition(Type a[], int pivot, int r)
 {
-    int i = p, j = r+1;
-    Type x = a[p];
+    int i = pivot, j = r+1;
+    Type x = a[pivot];                  // 将 pivot 处的元素挖出来 
     while(1)
     {
-        while(a[++i] < x && i < r);
-        while(a[--j] > x);
-        if(i >= j) break;
-        Swap(a[i], a[j]);
+        while(a[++i] < x && i < r);     // 保证 i 与 j 不会越界
+        while(a[--j] > x);     
+        if(i >= j) break;               // i 与 j 发生交叉，找到了基准元素应在的位置
+        else Swap(a[i], a[j]);          // 未发生交叉，则应当交换这两个元素，使
     }
-    a[p] = a[j];
-    a[j] = x;
-    return j;
+    a[pivot] = a[j];                    // 将占有 pivot 应当存放的位置上的元素挖出来，并填到现在 pivot 元素的位置
+    a[j] = x;                           // 将之前挖出来的 pivot 元素填到上一步挖出来的坑上
+    return j;                           // 返回此时 pivot 元素的下标
 }
 
 template<class Type>
-void QuickSort(Type a[], int p, int r)
+void QuickSort(Type a[], int l, int r)
 {
-    if(p < r)
+    if(l < r)       // 保证符合实际意义，不符合时 “什么也不做 ”（相当于 return）
     {
-        int q = Partition(a, p, r);
-        QuickSort(a, p, q-1);
+        int q = Partition(a, l, r);
+        QuickSort(a, l, q-1);
         QuickSort(a, q+1, r);
     }
 }
@@ -44,9 +44,9 @@ void QuickSort(Type a[], int p, int r)
 
 int main()
 {
-    int a[] = {2, 3, 7, 4, 9, 0, 1};
-    QuickSort(a, 0, 6);
-    for(int i = 0; i< 7; i++)
+    int a[] = {2, 3, 7, 30, 4, 9, 0, -1};
+    QuickSort(a, 0, 7);                 // 0 ~ N-1
+    for(int i = 0; i< 8; i++)
     {
         printf("%d ", a[i]);
     }
